677f5216691ddb8c9a2f7e57f78338c2586c9747
vinic9 <vinicmelo12@gmail.com>
2022-03-22 13:39:02 -0300

    Updating

--- ratata/main_js/main.js
+++ ratata/main_js/main.js
@@ -1,3 +1,21 @@
+//botão heading / esconder mostrar
+
+
+var btn = document.querySelector('#hidden0_btn');
+var first_panel = document.querySelector('.first-panel');
+
+
+btn.addEventListener('click', function()
+{
+  if (first_panel.style.display === "none") {
+    first_panel.style.display = "block";
+  } else {
+    first_panel.style.display = "none";
+  }
+});
+
+
+
 //botão pix / esconder mostrar
 
 
