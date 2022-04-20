b30d8dd583f5ea70ceb4dbd50a54ce3bd944ab42
vinic9 <vinimelo12@hotmail.com>
2022-04-20 15:17:59 -0300

    Copiado! Upload

--- invoice_pix_local/main_js/main.js
+++ invoice_pix_local/main_js/main.js
@@ -1,11 +1,16 @@
 //copy function
-document.getElementById("btn_copy").onclick = function (){
+document.getElementById("btn_copy").onclick = function () {
   var copy_text = document.getElementById("pix_number");
   copy_text.select();
   copy_text.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(copy_text.value);
 
-  document.getElementById("btn_copy").value = "Copiado!";
+  if (this.innerText === 'Pix Copia e Cola') {
+    this.innerText = 'Copiado';
+  }
+  else {
+    //
+  }
 
 
 }
