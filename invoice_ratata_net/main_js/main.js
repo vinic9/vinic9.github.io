5434b0918edd4cae230f0eb920bbf54370a5419d
vinic9 <vinimelo12@hotmail.com>
2022-04-20 16:06:17 -0300

    Code Update

--- invoice_ratata_net/main_js/main.js
+++ invoice_ratata_net/main_js/main.js
@@ -57,12 +57,19 @@ function autoTab(fromTextBox, toTextBox) {
 }
 
 
-//copy paste 
+//copy function
+document.getElementById("copy_btn").onclick = function () {
+  var copy_text = document.getElementById("pix_number");
+  copy_text.select();
+  copy_text.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copy_text.value);
 
-    function CopyButton(){
-      var copy_text = document.getElementById("pix_number");
-      copy_text.select();
-      copy_text.setSelectionRange(0, 99999);
-      navigator.clipboard.writeText(copy_text.value);
+  if (this.innerText === 'Pix Copia e Cola') {
+    this.innerText = 'Copiado';
+  }
+  else {
+    //
+  }
 
-    }
+
+}
