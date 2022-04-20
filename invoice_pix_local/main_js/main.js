7c623e54917a1f011f4aac39a1f345626a99af0c
vinic9 <vinimelo12@hotmail.com>
2022-04-20 13:51:06 -0300

    Adding button feedback
    Button Feedback = Adicionado
    Valor Individual = Deletado

--- invoice_pix_local/main_js/main.js
+++ invoice_pix_local/main_js/main.js
@@ -1,34 +1,12 @@
-//botão heading / esconder mostrar
+//copy function
+document.getElementById("btn_copy").onclick = function (){
+  var copy_text = document.getElementById("pix_number");
+  copy_text.select();
+  copy_text.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copy_text.value);
+
+  document.getElementById("btn_copy").value = "Copiado!";
 
 
-var btn = document.querySelector('#firstpanel_btn');
-var first_panel = document.querySelector('.first_panel');
+}
 
-
-btn.addEventListener('click', function () {
-  if (first_panel.style.display === "none") {
-    first_panel.style.display = "block";
-  } else {
-    first_panel.style.display = "none";
-  }
-});
-
-//Copy Function
-
-/*
-  if 'button' is 'active(true)'
-    then execute
-  Copy
-    seleect the the html tag 
-    select the attribute
-    select the value attribute
-    use function to copy it   
-  */
-
-    function CopyButton(){
-      var copy_text = document.getElementById("pix_number");
-      copy_text.select();
-      copy_text.setSelectionRange(0, 99999);
-      navigator.clipboard.writeText(copy_text.value);
-
-    }
